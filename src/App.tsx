b8c584819448c73059a1922fd553485bdb95741c
yoeldovidcohen <46816177+yoeldovidcohen@users.noreply.github.com>
2023-05-14 16:47:04 +0100

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { atom, useAtom } from "jotai";
 import { TodoList } from "./components/TodoList";
 import { AddItem } from "./components/AddItem";
 
