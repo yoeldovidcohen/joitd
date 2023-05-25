2a751bd82566b112a27b4aef5b806e42b7993c8c
yoeldovidcohen <46816177+yoeldovidcohen@users.noreply.github.com>
2023-05-25 01:08:58 +0100

    ri

---
+++ src/components/RemindItem.tsx
@@ -0,0 +1,26 @@
+import { useCallback, useState } from "react";
+import { useSelector } from "../store/store";
+import { useInterval, useTitle } from "react-use";
+
+const RemindItem = () => {
+  const [now, setNow] = useState(0)
+
+  const itemsList = useSelector(
+    useCallback(
+      (state) =>
+        state.todos.filter((todo) => !todo.completed && todo.dueAt && todo.dueAt < now),
+      [now]
+    )
+  );
+  useInterval(() => {
+    setNow(Date.now())
+  }, 1000);
+//   useTitle(itemsList.length ? `${itemsList.length.toString()} todos` : "todos")
+  return (
+    <>
+      <p>there are {itemsList.length ? itemsList.length.toString() : "no"} overdue items</p>
+    </>
+  );
+};
+
+export default RemindItem;
