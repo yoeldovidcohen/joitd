733eeb35d17d03ef68330848f78849e28e4f92b7
yoeldovidcohen <yoeldovidcohen@gmail.com>
2023-05-30 14:58:27 +0100

    tryshare

--- src/components/WebShare.tsx
+++ src/components/WebShare.tsx
@@ -2,6 +2,15 @@ const WebShare = () => {
   return (
     <>
       <p>web share</p>
+      <button
+        onClick={() => {
+          if (navigator.share && navigator.canShare()) {
+            navigator.share({ text: "hello" });
+          }
+        }}
+      >
+        try share
+      </button>
     </>
   );
 };
