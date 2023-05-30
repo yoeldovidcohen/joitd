639da846ab53bf27204fdf39137c8391ff2b737f
yoeldovidcohen <yoeldovidcohen@gmail.com>
2023-05-30 12:00:36 +0100

    alarmwork

--- src/components/PlayAlarm.tsx
+++ src/components/PlayAlarm.tsx
@@ -5,10 +5,10 @@ import { playingState } from "../store/playing";
 export const PlayAlarm = () => {
   const [shouldPlay, setShouldPlay] = useAtom(playingState);
   const [audio, state, controls, ref] = useAudio({
-    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
     autoPlay: true,
   });
-  useInterval(() => setShouldPlay(false), 10000);
+  useInterval(() => setShouldPlay(false), 11000);
 
   return (
     <>
