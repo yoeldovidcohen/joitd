e75753eb0cda57b4cbe0088a604d069f5d84b2a0
yoeldovidcohen <46816177+yoeldovidcohen@users.noreply.github.com>
2023-06-08 16:46:43 +0100

    arrows ok

--- src/components/DateCom.tsx
+++ src/components/DateCom.tsx
@@ -26,19 +26,50 @@ const DateCome = () => {
   ];
   return (
     <>
-      <div className="m-2 md:m-4 lg:m-5 p-2 md:p-4 lg:p-5 text-center flex flex-col md:flex-row gap-1 md:gap-4 lg:gap-5 justify-center align-middle justify-items-center items-center	">
-        <p className="text-6xl md:text-8xl lg:text-9xl order-1 md:order-2">
-          {theDate.getDate()}
-        </p>
-        <p className="text-l md:text-xl lg:text-xl text-gray-400 order-2 md:order-1">
-          {days[theDate.getDay()]}
-        </p>
-        <p className="text-4xl md:text-6xl lg:text-7xl order-3">
-          {months[theDate.getMonth()]}
-        </p>
-        <p className="text-l md:text-xl lg:text-xl text-gray-400 order-4">
-          {theDate.getFullYear()}
-        </p>
+      <div className="min-w-[90vw] flex flex-row justify-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 my-auto"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"
+          />
+        </svg>
+
+        <div className="m-2 md:m-4 lg:m-5 p-2 md:p-4 lg:p-5 text-center flex flex-col md:flex-row gap-1 md:gap-4 lg:gap-5 justify-center align-middle justify-items-center items-center	">
+          <p className="text-6xl md:text-8xl lg:text-9xl order-1 md:order-2">
+            {theDate.getDate()}
+          </p>
+          <p className="text-l md:text-xl lg:text-xl text-gray-400 order-2 md:order-1">
+            {days[theDate.getDay()]}
+          </p>
+          <p className="text-4xl md:text-6xl lg:text-7xl order-3">
+            {months[theDate.getMonth()]}
+          </p>
+          <p className="text-l md:text-xl lg:text-xl text-gray-400 order-4">
+            {theDate.getFullYear()}
+          </p>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 my-auto"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8.25 4.5l7.5 7.5-7.5 7.5"
+          />
+        </svg>
       </div>
     </>
   );
