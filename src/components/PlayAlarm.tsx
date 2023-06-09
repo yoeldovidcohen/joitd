163354ce8f36cb7f40134ddbb8ad9e864fb3d2e0
yoeldovidcohen <46816177+yoeldovidcohen@users.noreply.github.com>
2023-06-09 13:52:53 +0100

    diff alarm

--- src/components/PlayAlarm.tsx
+++ src/components/PlayAlarm.tsx
@@ -1,8 +1,32 @@
 import { useAtom } from "jotai";
 import { useAudio, useInterval } from "react-use";
 import { playingState } from "../store/playing";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useSelector } from "../store/store";
+import { isSameMinute } from "date-fns";
 
 export const PlayAlarm = () => {
+  const [dateTime, setDateTime] = useState(new Date());
+  function refreshClock() {
+    setDateTime(new Date());
+  }
+  useEffect(() => {
+    const timerId = setInterval(refreshClock, 1000);
+    return function cleanup() {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  const playList = useSelector(
+    useCallback(
+      (state) =>
+        state.todos.find(
+          (todo) => todo.dueAt && isSameMinute(dateTime, new Date(todo.dueAt))
+        ),
+      [dateTime]
+    )
+  );
+
   const [shouldPlay, setShouldPlay] = useAtom(playingState);
   const intervalAction = () => {
     setShouldPlay(false);
@@ -17,7 +41,7 @@ export const PlayAlarm = () => {
   return (
     <>
       <p>alarm</p>
-      {shouldPlay && (
+      {playList && dateTime.getSeconds() > 0 && dateTime.getSeconds() < 10 && (
         <>
           {audio}
           <p>alarm ringing</p>
