ca5e123793191dd556d2f7ae9e99da80b7d6218b
yoeldovidcohen <46816177+yoeldovidcohen@users.noreply.github.com>
2023-06-16 13:27:14 +0100

    emoji

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "../store/store";
 import { useAudio, useInterval } from "react-use";
 import { playingState } from "../store/playing";
 import { useAtom } from "jotai";
+import FaceCome from "./FaceCome";
 
 export const TodoItem = ({ id }: { id: string }) => {
   const dispatch = useDispatch();
@@ -42,8 +43,10 @@ export const TodoItem = ({ id }: { id: string }) => {
         />
         {!editMode && (
           <p
-            style={{ textDecoration: item?.completed ? "line-through" : "" }}
-            className={`block ${editMode ? "bg-green-200" : "bg-red-200"}`}
+            // style={{ textDecoration: item?.completed ? "line-through" : "" }}
+            className={`block ${editMode ? "bg-green-200" : "bg-red-200"} ${
+              item?.completed ? "line-through" : ""
+            }`}
           >
             {item?.title}
           </p>
@@ -102,11 +105,11 @@ export const TodoItem = ({ id }: { id: string }) => {
             <p className="block">{itemDate.toLocaleTimeString().slice(0, 5)}</p>
           </>
         )}
-        {isOverdue && !item?.completed && (
+        {/* {isOverdue && !item?.completed && (
           <>
             <p>overdue</p>
           </>
-        )}
+        )} */}
 
         {/* {item?.dueAt && (
         <>
@@ -114,7 +117,57 @@ export const TodoItem = ({ id }: { id: string }) => {
           <span>{item.dueAt.getTime()}</span>
         </>
       )} */}
-        <button onClick={remove}>remove</button>
+        {/* <button onClick={remove}>remove</button> */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+          onClick={remove}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+          />
+        </svg>
+        <FaceCome completed={item?.completed} isOverdue={isOverdue} />
+        {/* {isOverdue && !item?.completed && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            // stroke="currentColor"
+            className="w-6 h-6 stroke-red-500"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+            />
+          </svg>
+        )}
+        {!isOverdue && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            // stroke="currentColor"
+            className={`w-6 h-6 ${
+              item?.completed ? "stroke-green-500" : "stroke-current"
+            }`}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+            />
+          </svg>
+        )} */}
       </div>
     </>
   );
